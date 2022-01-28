67f3a0b04391fd5c0963ca018564a7ed28139793
Cyber$killz <casinadie256@hotmail.com>
2022-01-28 12:40:18 -0500

    Ejercicios básicos Python
    Ejercicios de bucles , funciones variables .etc

---
+++ EjerciciosPython2/Problema2.py
@@ -0,0 +1,5 @@
+from os import system 
+system ("cls")
+
+sueldos = ["190000" "289000" "120000"]
+for x 
---
+++ EjerciciosPython2/bucle_while.py
@@ -0,0 +1,9 @@
+from os import system
+system ("cls")
+merca_="true"
+listademercado = ["apple", "banana", "cherry", "apple", "cherry"]
+pregunta = input("ingrese un elemento a la lista :")
+listademercado.append(pregunta)
+print (listademercado);
+while (listademercado)=>0:
+    print (listademercado)
